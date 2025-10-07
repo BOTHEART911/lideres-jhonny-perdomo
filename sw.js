4b6c3429b48d2e093e114b3ca43b03c326296388
Oscar Polania <opolania11@gmail.com>
2025-10-07 17:22:27 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-
-// Red primero; si falla, intenta caché (si existe)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
